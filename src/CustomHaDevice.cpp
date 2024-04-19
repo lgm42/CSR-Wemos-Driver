7650f466c10ac8a00de7eff89a353576b14aef6a
BuildTools <unconfigured@null.spigotmc.org>
2024-04-19 19:35:57 +0200

    ✨ Added entities inside CustomHaDevice and it works on HA

--- src/CustomHaDevice.cpp
+++ src/CustomHaDevice.cpp
@@ -3,15 +3,17 @@
 #include "CustomHaDevice.h"
 #include "settings.h"
 
-CustomHaDevice::CustomHaDevice(String uniqueId, Client& client) : 
-    _uniqueId(uniqueId),
-    _device(_uniqueId.c_str()),
-    _mqtt(client, _device),
+CustomHaDevice::CustomHaDevice() : 
+    _mqtt(nullptr),
+    _device(nullptr),
     _networkFrequency(0),
-    _networkFrequencyName(_uniqueId + String("_ac_frequency")),
-    _networkFrequencyEntity("network_frequency", HABaseDeviceType::PrecisionP2), //_networkFrequencyName.c_str()
-    _uptimeSensorEntity("Uptime"),
-    _last_report(0)
+    _last_report(0),
+    _setPointValue(0.f),
+    _networkFrequencyEntity("CSR-dev1-network_frequency", HABaseDeviceType::PrecisionP2),
+    _uptimeSensorEntity("CSR-dev1-uptime"),
+    _powerSetPoint("CSR-dev1-power_setpoint"),
+
+    _testNumber("CSR-dev1-test_number")
 {
 }
 
@@ -21,36 +23,88 @@ CustomHaDevice::~CustomHaDevice()
 
 void CustomHaDevice::setup()
 {
+    String uniqueId = String("CSR-dev2-_") + WiFi.macAddress();
+    uniqueId.replace(":", "");
+    uniqueId.toLowerCase();
+    _uniqueId = uniqueId;
+
+    _device = new HADevice(_uniqueId.c_str());
+
     Log.printf("Setup Ha device\n\r");
-    _device.setName("CSR Wemos Driver");
-    _device.setSoftwareVersion(VERSION);
-    _mqtt.begin(BROKER_ADDR, BROKER_USER, BROKER_PASSWORD);
+    _device->setName("CSR Wemos Driver-dev2");
+    _device->setSoftwareVersion(VERSION);
     
+    _mqtt = new HAMqtt(_wifiClient, *_device);
+
+
+    Log.printf("Configurating entities...\n\r");
+
     _networkFrequencyEntity.setIcon("mdi:sine-wave");
     _networkFrequencyEntity.setName("Network frequency");
     _networkFrequencyEntity.setUnitOfMeasurement("Hz");
     _networkFrequencyEntity.setDeviceClass("FREQUENCY");
-    //_networkFrequencyEntity.setMode(HANumber::ModeBox);
+    _mqtt->addDeviceType(&_networkFrequencyEntity);
+
     _uptimeSensorEntity.setIcon("mdi:timer-outline");
     _uptimeSensorEntity.setName("Uptime");
     _uptimeSensorEntity.setUnitOfMeasurement("s");
+    _mqtt->addDeviceType(&_uptimeSensorEntity);
+
+    _powerSetPoint.onCommand(CustomHaDevice::onSetPointCommand);
+    _powerSetPoint.setIcon("mdi:flash");
+    _powerSetPoint.setName("Power SetPoint2");
+    _powerSetPoint.setUnitOfMeasurement("W");
+    _powerSetPoint.setDeviceClass("POWER");
+    _powerSetPoint.setMin(0);
+    _powerSetPoint.setMax(4000);
+    _setPointValue = 0.f;
+    _mqtt->addDeviceType(&_powerSetPoint);
+
+    //_powerSetPoint->setAvailability(true);
+
+    _testNumber.onCommand(CustomHaDevice::onSetPointCommand);
+    _testNumber.setIcon("mdi:home");
+    _testNumber.setName("My number");
+    _mqtt->addDeviceType(&_testNumber);
 
     _last_report = millis();
+
+    Log.printf("Connecting to MQTT...\n\r");
+    _mqtt->begin(BROKER_ADDR, BROKER_USER, BROKER_PASSWORD);
+    Log.printf("Ha Device setup finished.\n\r");
 }
 
 void CustomHaDevice::loop()
 {
-    _mqtt.loop();
+    _mqtt->loop();
     if (millis() - _last_report < 5 * 1000)
         return;
 
-    Log.printf("Reporting network Frequency value\n\r");
+    Log.printf("Reporting network Frequency value: %f\n\r", _networkFrequency);
     _networkFrequencyEntity.setValue(_networkFrequency);
     _uptimeSensorEntity.setValue(millis() / 1000.f);
     _last_report = millis();
+}
+
+float CustomHaDevice::setPoint() const
+{
+    return _setPointValue;
 }
 
 void CustomHaDevice::networkFrequency(float value)
 {
     _networkFrequency = value;
 }
+
+void CustomHaDevice::onSetPointCommand(HANumeric number, HANumber* sender)
+{
+    if (!number.isSet())
+        HaDevice._setPointValue = 0;
+    HaDevice._setPointValue = number.toFloat();
+    Log.printf("onSetPointCommand, setpoint set to %f -> %f\n\r", sender->getCurrentState().toFloat(), HaDevice._setPointValue);
+    sender->setState(HaDevice._setPointValue);
+}
+
+#if !defined(NO_GLOBAL_INSTANCES) 
+CustomHaDevice HaDevice;
+#endif
--- src/CustomHaDevice.h
+++ src/CustomHaDevice.h
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Ethernet.h>
 
 #include <HADevice.h>
 #include <HAMqtt.h>
@@ -9,20 +11,33 @@
 class CustomHaDevice
 {
     public:
-        CustomHaDevice(String uniqueId, Client & client);
+        CustomHaDevice();
         virtual ~CustomHaDevice();
 
         void setup();
         void loop();
 
         void networkFrequency(float value);
+        float setPoint() const;
+
     private:
+        WiFiClient _wifiClient;
         String _uniqueId;
-        HADevice _device;
-        HAMqtt _mqtt;
+        HADevice *_device;
+        HAMqtt *_mqtt;
         float _networkFrequency;
-        String _networkFrequencyName;
         HASensorNumber _networkFrequencyEntity;
         HASensorNumber _uptimeSensorEntity;
+        HANumber _powerSetPoint;
+        HANumber _testNumber;
+
         unsigned long _last_report;
+        float _setPointValue;
+
+        static void onSetPointCommand(HANumeric number, HANumber* sender);
+
 };
+
+#if !defined(NO_GLOBAL_INSTANCES)
+extern CustomHaDevice HaDevice;
+#endif
